2bfdacad13fb0d1e2f1e2962814aeb1dd646eb13
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2024-06-10 15:54:17 +0500

    write nothing if not wdb_ok

--- src/wdb.c
+++ src/wdb.c
@@ -3,14 +3,21 @@
 #include <string.h> /* for strlen */
 #include <stdio.h> /* for snprintf */
 
-/* undef at the bottom */
+#define SOB_WDB_CHECK_RESTORE_LEN_(stmt) \
+    do { \
+        const enum wdb_res SOB_WDB_CHECK_res_ = (stmt); \
+        if (SOB_WDB_CHECK_res_ != wdb_ok) { \
+            c->len = last_len; \
+            return SOB_WDB_CHECK_res_; \
+        } \
+    } while (0)
 #define SOB_WDB_CHECK_KEY_() \
     do { \
         if (! c->got_key) { \
             return wdb_syntax; \
         } \
     } while (0)
-#define SOB_WDB_MAYBE_ARR_() SOB_WDB_CHECK(maybe_arr_(c))
+#define SOB_WDB_MAYBE_ARR_() SOB_WDB_CHECK_RESTORE_LEN_(maybe_arr_(c))
 
 struct wdb_ctx {
     char * out;
@@ -44,42 +51,46 @@ const char * wdb_out_str(const struct wdb_ctx * c)
 
 enum wdb_res wdb_key(struct wdb_ctx * c, const char * v)
 {
+    size_t last_len = c->len;
     if ((! c->got_key) || (! c->is_first_val)) {
         size_t i;
 
         if (c->got_key) {
-            SOB_WDB_CHECK(add_ch_(c, '\n'));
+            SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '\n'));
         }
         for (i = 0; i < strlen(v); i++) {
             char ch = v[i];
             if (! ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
                         || (ch >= '0' && ch <= '9')
                         || ch == '.' || ch == '_' || ch == '-')) {
+                c->len = last_len;
                 return wdb_syntax;
             }
         }
 
-        SOB_WDB_CHECK(add_literal_(c, v));
-        SOB_WDB_CHECK(add_literal_(c, ": "));
+        SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, v));
+        SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, ": "));
 
         c->got_key = 1;
         c->is_first_val = 1;
 
         return wdb_ok;
     } else {
+        c->len = last_len;
         return wdb_syntax;
     }
 }
 
 enum wdb_res wdb_str(struct wdb_ctx * c, const char * v)
 {
+    size_t last_len = c->len;
     size_t i;
 
     SOB_WDB_CHECK_KEY_();
     SOB_WDB_MAYBE_ARR_();
     c->is_first_val = 0;
 
-    SOB_WDB_CHECK(add_ch_(c, '"'));
+    SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '"'));
     for (i = 0; i < strlen(v); i++) {
         char ch = v[i];
         int is_escape = 0;
@@ -99,30 +110,32 @@ enum wdb_res wdb_str(struct wdb_ctx * c, const char * v)
             default:
                 if ((ch <= 31 || ch >= 127) && ch != '\t') {
                     /* control character */
+                    c->len = last_len;
                     return wdb_syntax;
                 }
                 break;
         };
         if (is_escape ) {
-            SOB_WDB_CHECK(add_ch_(c, '\\'));
+            SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '\\'));
             /* ch was modified */
         }
-        SOB_WDB_CHECK(add_ch_(c, ch));
+        SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, ch));
     }
-    SOB_WDB_CHECK(add_ch_(c, '"'));
+    SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '"'));
 
     return wdb_ok;
 }
 
 enum wdb_res wdb_long_str(struct wdb_ctx * c, const char * v)
 {
+    size_t last_len = c->len;
     size_t i;
 
     SOB_WDB_CHECK_KEY_();
     SOB_WDB_MAYBE_ARR_();
     c->is_first_val = 0;
 
-    SOB_WDB_CHECK(add_literal_(c, "<\n"));
+    SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, "<\n"));
     for (i = 0; i < strlen(v); i++) {
         char ch = v[i];
         int is_escape = 0;
@@ -138,23 +151,25 @@ enum wdb_res wdb_long_str(struct wdb_ctx * c, const char * v)
             default:
                 if ((ch <= 31 || ch >= 127) && ch != '\t' && ch != '\n') {
                     /* control character */
+                    c->len = last_len;
                     return wdb_syntax;
                 }
                 break;
         };
         if (is_escape) {
-            SOB_WDB_CHECK(add_ch_(c, '\\'));
+            SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '\\'));
             /* ch was modified */
         }
-        SOB_WDB_CHECK(add_ch_(c, ch));
+        SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, ch));
     }
-    SOB_WDB_CHECK(add_literal_(c, "\n>"));
+    SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, "\n>"));
 
     return wdb_ok;
 }
 
 enum wdb_res wdb_int(struct wdb_ctx * c, long v)
 {
+    size_t last_len = c->len;
     char buf[50];
     int written;
 
@@ -164,13 +179,16 @@ enum wdb_res wdb_int(struct wdb_ctx * c, long v)
 
     written = snprintf(buf, sizeof(buf), "%li", v);
     if (written >= sizeof(buf)) {
+        c->len = last_len;
         return wdb_overflow;
     }
-    return add_literal_(c, buf);
+    SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, buf));
+    return wdb_ok;
 }
 
 enum wdb_res wdb_num(struct wdb_ctx * c, double v)
 {
+    size_t last_len = c->len;
     char buf[100];
     int written;
 
@@ -180,26 +198,32 @@ enum wdb_res wdb_num(struct wdb_ctx * c, double v)
 
     written = snprintf(buf, sizeof(buf), "%f", v);
     if (written >= sizeof(buf)) {
+        c->len = last_len;
         return wdb_overflow;
     }
-    return add_literal_(c, buf);
+    SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, buf));
+    return wdb_ok;
 }
 
 enum wdb_res wdb_bool(struct wdb_ctx * c, int v)
 {
+    size_t last_len = c->len;
     SOB_WDB_CHECK_KEY_();
     SOB_WDB_MAYBE_ARR_();
     c->is_first_val = 0;
-
-    return add_literal_(c, v ? "true" : "false");
+    SOB_WDB_CHECK_RESTORE_LEN_(add_literal_(c, v ? "true" : "false"));
+    return wdb_ok;
 }
 
 enum wdb_res wdb_fin(struct wdb_ctx * c) {
+    size_t last_len = c->len;
     if (c->got_key && c->is_first_val) {
+        c->len = last_len;
         return wdb_syntax;
     } else {
-        SOB_WDB_CHECK(add_ch_(c, '\n'));
-        return add_ch_(c, '\0');
+        SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '\n'));
+        SOB_WDB_CHECK_RESTORE_LEN_(add_ch_(c, '\0'));
+        return wdb_ok;
     }
 }
 
@@ -245,11 +269,13 @@ static enum wdb_res add_ch_(struct wdb_ctx * c, char ch)
         if (MY_WDB_CHECK_res_ != wdb_ok) { \
             printf("err @ %s (%i): %i\n", \
                 #stmt, __LINE__, MY_WDB_CHECK_res_); \
+            printf("'%s'\n", wdb_out_str(&c)); \
+            return 1; \
         } \
     } while (0)
 
 enum {
-    str_mlen_ = 1024
+    str_mlen_ = 4
 };
 
 int main(void) {
